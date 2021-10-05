909bbc1140616d9b8fc9f7dcd510491c0b059bce
karenvalencia-bit <karenochoa196@gmail.com>
2021-10-05 18:00:53 -0500

    martes virtual

--- controlador.js
+++ controlador.js
@@ -1,35 +1,190 @@
+//medico
 let medico1=document.getElementById("medico1");
 let fotomedico1=document.getElementById("fotomedico1");
 let medico2=document.getElementById("medico2");
 let fotomedico2=document.getElementById("fotomedico2");
 
+//constructor
+let constructor1=document.getElementById("constructor1");
+let fotoconstructor1=document.getElementById("fotoconstructor1");
+let constructor2=document.getElementById("constructor2");
+let fotoconstructor2=document.getElementById("fotoconstructor2");
+
+//profesor
+let profesor1=document.getElementById("profesor1");
+let fotoprofesor1=document.getElementById("fotoprofesor1");
+let profesor2=document.getElementById("profesor2");
+let fotoprofesor2=document.getElementById("fotoprofesor2");
+
+
+let contadormedico=0;
+let contadorconstructor=0;
+let contadorprofesor=0;
+
 
 medico1.addEventListener("click", activarmedico1);
 medico2.addEventListener("click", activarmedico2);
+
+constructor1.addEventListener("click", activarconstructor1);
+constructor2.addEventListener("click", activarconstructor2);
+
+profesor1.addEventListener("click", activarprofesor1);
+profesor2.addEventListener("click", activarprofesor2);
+
 
 
 function activarmedico1(){
 
     
-    medico1.classList.remove("bg-dark");
+    medico1.classList.remove("bg-warning");
     fotomedico1.classList.remove("invisible");
+    contadormedico++;
 
     setTimeout(function() {
+
+        if(contadormedico !=2){
+            medico1.classList.add("bg-warning");
+           fotomedico1.classList.add("invisible");
+           contadormedico--;
+
+        }
+        else{
+            let audio1 = new Audio("audio/audio1.mp3");
+            audio1.play();
+
+            let resultadoMedico=document.getElementById("resultadoMedico");
+            resultadoMedico.classList.remove("invisible");
+            resultadoMedico.classList.add("visible");
+        }
  
-        medico1.classList.add("bg-dark");
-    fotomedico1.classList.add("invisible");
+        
     },1000)
 }
+
 
 function activarmedico2(){
 
     
-    medico2.classList.remove("bg-dark");
+    medico2.classList.remove("bg-primary");
     fotomedico2.classList.remove("invisible");
+    contadormedico++;
 
     setTimeout(function() {
  
-        medico2.classList.add("bg-dark");
-    fotomedico2.classList.add("invisible");
+        if(contadormedico !=2){
+            medico2.classList.add("bg-primary");
+           fotomedico2.classList.add("invisible");
+           contadormedico--;
+
+        }
+        else{
+            let audio1 = new Audio("audio/audio1.mp3");
+            audio1.play();
+            let resultadoMedico=document.getElementById("resultadoMedico");
+            resultadoMedico.classList.remove("invisible");
+            resultadoMedico.classList.add("visible");
+        }
     },1000)
+}
+
+function activarconstructor1(){
+
+    
+    constructor1.classList.remove("bg-secondary");
+    fotoconstructor1.classList.remove("invisible");
+    contadorconstructor++;
+
+    setTimeout(function() {
+ 
+        if(contadorconstructor !=2){
+            constructor1.classList.add("bg-secondary");
+           fotoconstructor1.classList.add("invisible");
+           contadorconstructor--;
+
+        }
+        else{
+            let audio1 = new Audio("audio/audio1.mp3");
+            audio1.play();
+            let resultadoConstructor=document.getElementById("resultadoConstructor");
+            resultadoConstructor.classList.remove("invisible");
+            resultadoConstructor.classList.add("visible");
+        }
+    },1000)
+}
+
+
+function activarconstructor2(){
+
+    
+    constructor2.classList.remove("bg-dark");
+    fotoconstructor2.classList.remove("invisible");
+    contadorconstructor++;
+
+    setTimeout(function() {
+ 
+        if(contadorconstructor !=2){
+            constructor2.classList.add("bg-dark");
+           fotoconstructor2.classList.add("invisible");
+           contadorconstructor--;
+
+        }
+        else{
+            let audio1 = new Audio("audio/audio1.mp3");
+            audio1.play();
+            let resultadoConstructor=document.getElementById("resultadoConstructor");
+            resultadoConstructor.classList.remove("invisible");
+            resultadoConstructor.classList.add("visible");
+
+        }
+    },1000)
+}
+
+function activarprofesor1(){
+
+    
+    profesor1.classList.remove("bg-danger");
+    fotoprofesor1.classList.remove("invisible");
+    contadorprofesor++;
+
+    setTimeout(function() {
+ 
+        if(contadorprofesor !=2){
+            profesor1.classList.add("bg-danger");
+           fotoprofesor1.classList.add("invisible");
+           contadorprofesor--;
+
+        }
+        else{
+            let audio1 = new Audio("audio/audio1.mp3");
+            audio1.play();
+            let resultadoProfesor=document.getElementById("resultadoProfesor");
+            resultadoProfesor.classList.remove("invisible");
+            resultadoProfesor.classList.add("visible");
+        }
+    },1000)
+}
+
+function activarprofesor2(){
+
+    profesor2.classList.remove("bg-danger");
+    fotoprofesor2.classList.remove("invisible");
+    contadorprofesor++;
+
+    setTimeout(function() {
+ 
+        if(contadorprofesor !=2){
+            profesor2.classList.add("bg-danger");
+           fotoprofesor2.classList.add("invisible");
+           contadorprofesor--;
+
+        }
+        else{
+            let audio1 = new Audio("audio/audio1.mp3");
+            audio1.play();
+            let resultadoProfesor=document.getElementById("resultadoProfesor");
+            resultadoProfesor.classList.remove("invisible");
+            resultadoProfesor.classList.add("visible");
+        }
+    },1000)
+   
 }
